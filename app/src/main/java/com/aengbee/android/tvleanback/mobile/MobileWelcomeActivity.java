7bbf3d0e072f9b331fba0c84d24ea745d60181de
xavaz <fytoz@hotmail.com>
2017-03-12 05:43:22 +0900

    image change

---
+++ app/src/main/java/com/aengbee/android/tvleanback/mobile/MobileWelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.aengbee.android.tvleanback.mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.aengbee.android.tvleanback.R;
+
+public class MobileWelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mobile_welcome);
+    }
+}
